ff676ed5868789c3439139d7f3bf968c7c5aa3a1
David Wilbert <dwilbert@rhrk.uni-kl.de>
2021-12-18 03:12:49 +0100

    keine ahnung was da passiert, aber es sieht aus als würde die List.add nicht funktionieren oder so

--- web-chess-backend/src/main/java/de/sollder1/webchess/backend/game/engine/figures/Bishop.java
+++ web-chess-backend/src/main/java/de/sollder1/webchess/backend/game/engine/figures/Bishop.java
@@ -15,11 +15,10 @@ public class Bishop extends Figure {
 
     @Override
     public boolean isMoveValid(Move move, byte[][] gameField, boolean kingInCheck) {
+        return getValidMoves(move.getFrom(), gameField, kingInCheck).contains(move.getTo());
+    }
+        /*
         byte currentFigure = gameField[move.getFrom().getX()][move.getFrom().getY()];
-
-        if (!(currentFigure == Figure.BISHOP_BLACK || currentFigure == Figure.BISHOP_WHITE)) {
-            return false;
-        }
 
         byte x = 1;
         byte y = 1;
@@ -54,10 +53,98 @@ public class Bishop extends Figure {
         return false;
 
     }
+    */
 
 
     @Override
     public List<Coordinate> getValidMoves(Coordinate figurePosition, byte[][] gameField, boolean kingInCheck) {
-        return null;
+        List<Coordinate> validMoves = null;
+        byte figurePos = gameField[figurePosition.getX()][figurePosition.getY()];
+
+        byte x = (byte) (figurePosition.getX() - 1);
+        byte y = (byte) (figurePosition.getY() - 1);
+        while (x < FIELD_SIZE && x >= 0 && y < FIELD_SIZE && y >= 0) {
+            if (gameField[x][y] == EMPTY__FIELD) {
+                Coordinate nextCoordinate = new Coordinate(x, y);
+                validMoves.add(nextCoordinate);
+            }
+            else {
+                if (gameField[x][y] / Math.abs(gameField[x][y]) == figurePos / Math.abs(figurePos)) {
+                    break;
+                }
+                else {
+                    Coordinate nextCoordinate = new Coordinate(x, y);
+                    validMoves.add(nextCoordinate);
+                    break;
+                }
+            }
+            x--;
+            y--;
+        }
+
+        x = (byte) (figurePosition.getX() - 1);
+        y = (byte) (figurePosition.getY() + 1);
+        while (x < FIELD_SIZE && x >= 0 && y < FIELD_SIZE && y >= 0) {
+            if (gameField[x][y] == EMPTY__FIELD) {
+                Coordinate nextCoordinate = new Coordinate(x, y);
+                validMoves.add(nextCoordinate);
+            }
+            else {
+                if (gameField[x][y] / Math.abs(gameField[x][y]) == figurePos / Math.abs(figurePos)) {
+                    break;
+                }
+                else {
+                    Coordinate nextCoordinate = new Coordinate(x, y);
+                    validMoves.add(nextCoordinate);
+                    break;
+                }
+            }
+            x--;
+            y++;
+        }
+
+        x = (byte) (figurePosition.getX() + 1);
+        y = (byte) (figurePosition.getY() - 1);
+        while (x < FIELD_SIZE && x >= 0 && y < FIELD_SIZE && y >= 0) {
+            if (gameField[x][y] == EMPTY__FIELD) {
+                Coordinate nextCoordinate = new Coordinate(x, y);
+                validMoves.add(nextCoordinate);
+            }
+            else {
+                if (gameField[x][y] / Math.abs(gameField[x][y]) == figurePos / Math.abs(figurePos)) {
+                    break;
+                }
+                else {
+                    Coordinate nextCoordinate = new Coordinate(x, y);
+                    validMoves.add(nextCoordinate);
+                    break;
+                }
+            }
+            x++;
+            y--;
+        }
+
+        x = (byte) (figurePosition.getX() + 1);
+        y = (byte) (figurePosition.getY() + 1);
+        while (x < FIELD_SIZE && x >= 0 && y < FIELD_SIZE && y >= 0) {
+            if (gameField[x][y] == EMPTY__FIELD) {
+                Coordinate nextCoordinate = new Coordinate(x, y);
+                validMoves.add(nextCoordinate);
+            }
+            else {
+                if (gameField[x][y] / Math.abs(gameField[x][y]) == figurePos / Math.abs(figurePos)) {
+                    break;
+                }
+                else {
+                    Coordinate nextCoordinate = new Coordinate(x, y);
+                    validMoves.add(nextCoordinate);
+                    break;
+                }
+            }
+            x++;
+            y++;
+        }
+
+        return validMoves;
     }
 }
--- web-chess-backend/src/test/java/BishopTest.java
+++ web-chess-backend/src/test/java/BishopTest.java
@@ -19,14 +19,24 @@ public class BishopTest {
             {Figure.CASTLE_WHITE, Figure.KNIGHT_WHITE, Figure.BISHOP_WHITE, Figure.KING___WHITE, Figure.QUEEN__WHITE, Figure.BISHOP_WHITE, Figure.KNIGHT_WHITE, Figure.CASTLE_WHITE},
     };
 
+    private byte[][] customField = {
+            {Figure.CASTLE_BLACK, Figure.KNIGHT_BLACK, Figure.BISHOP_BLACK, Figure.KING___BLACK, Figure.QUEEN__BLACK, Figure.BISHOP_BLACK, Figure.KNIGHT_BLACK, Figure.CASTLE_BLACK},
+            {Figure.PAWN___BLACK, Figure.PAWN___BLACK, Figure.PAWN___BLACK, Figure.EMPTY__FIELD, Figure.PAWN___BLACK, Figure.PAWN___BLACK, Figure.PAWN___BLACK, Figure.PAWN___BLACK},
+            {Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.PAWN___BLACK, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD},
+            {Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD},
+            {Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD},
+            {Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD, Figure.EMPTY__FIELD},
+            {Figure.PAWN___WHITE, Figure.PAWN___WHITE, Figure.PAWN___WHITE, Figure.PAWN___WHITE, Figure.PAWN___WHITE, Figure.PAWN___WHITE, Figure.PAWN___WHITE, Figure.PAWN___WHITE},
+            {Figure.CASTLE_WHITE, Figure.KNIGHT_WHITE, Figure.BISHOP_WHITE, Figure.KING___WHITE, Figure.QUEEN__WHITE, Figure.BISHOP_WHITE, Figure.KNIGHT_WHITE, Figure.CASTLE_WHITE},
+    };
+
 
     @Test
     public void testMoves() {
 
         Coordinate from = new Coordinate((byte) 0, (byte) 2);
         Coordinate to = new Coordinate((byte) 2, (byte) 4);
-
-        assertFalse(FigureApi.getBehaviourModelById(Figure.BISHOP_BLACK).isMoveValid(new Move(from, to), startField, false));
+        assertFalse(FigureApi.getBehaviourModelById(Figure.BISHOP_BLACK).isMoveValid(new Move(from, to), customField, false));
 
 
     }
