f3ff0be135d83b632592956edd827f4d4f6b5a8c
sollder1 <124xyZWER#>
2021-12-21 20:44:51 +0100

    ...

--- web-chess-frontend/src/rest/model/LobbyToPlayerPayload.ts
+++ web-chess-frontend/src/rest/model/LobbyToPlayerPayload.ts
@@ -2,5 +2,7 @@ import PlayerPayload from "./PlayerPayload";
 
 export default interface LobbyToPlayerPayload {
     connected?: boolean,
-    player: PlayerPayload
+    player: PlayerPayload,
+    playerColor?: string,
+    yourTurn?: boolean
 }
